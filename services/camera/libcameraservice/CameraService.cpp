bb3875dd4ef8467a7800df754bde046aca4e1013
Marko Man <darkobas@gmail.com>
2019-11-05 19:03:47 +0100

    cameraservice: move client_package_name to data/misc
    Change-Id: I0d244b81625fd9514000bea313a28ad60321333f

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2550,7 +2550,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/bliss/client_package_name");
+    std::ofstream cpf("/data/misc/bliss/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif
